465fa1503b67678b872f5d21e73f99530d4a1011
David Mullally <mullallydavid7@gmail.com>
2023-02-11 15:48:38 +0100

    add custom middleware that shows errors in the console and the browser

--- server.js
+++ server.js
@@ -4,18 +4,45 @@ const app = express();
 
 const port = 3000;
 
-const apiRoutes = require("./routes/apiRoutes")
+const apiRoutes = require("./routes/apiRoutes");
 
 app.get("/", (req, res) => {
-   res.json({message: "API running..."})
+  console.log("synchronous code");
+  throw new Error("some error occured");
+  res.json({ message: "API running..." });
 });
 
-app.use('/api', apiRoutes)
+app.get("/a", (req, res, next) => {
+  setTimeout(() => {
+    try {
+      aconsole.log("asynchronous code");
+    } catch (err) {
+      next(err);
+    }
+  }, 1000)
+//next(new Error("some error occured"))
+});
+
+app.use("/api", apiRoutes);
 
 
-app.get('/api/products', (req, res) => {
-    res.send("Handling product routes.")
+//show errors in the console
+app.use((error, req, res, next) => {
+  console.log(error);
+  res.status(500).json({
+    message: error.message,
+    stack: error.stack
+  })
 })
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  next(error)
+})
+
+app.get("/api/products", (req, res) => {
+  res.send("Handling product routes.");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
