0a00d7d9cdd121d4dc0ed815c02fcbbfe5b2b8f0
David Mullally <mullallydavid7@gmail.com>
2023-02-14 23:23:58 +0100

    finish the product model

--- server.js
+++ server.js
@@ -2,35 +2,38 @@ const express = require("express");
 
 const app = express();
 
-const port = 3000;
+const port = 3001;
 
 const apiRoutes = require("./routes/apiRoutes");
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res, next) => {
+  const Product = require("./models/ProductModel");
+  try {
+  } catch (err) {
+    next(err);
+  }
   res.json({ message: "API running..." });
 });
 
-
 //mongodb connection
-const connectDB= require("./config/db")
+const connectDB = require("./config/db");
 connectDB();
 
 app.use("/api", apiRoutes);
-
 
 //show errors in the console
 app.use((error, req, res, next) => {
   console.log(error);
   res.status(500).json({
     message: error.message,
-    stack: error.stack
-  })
-})
+    stack: error.stack,
+  });
+});
 
 app.use((error, req, res, next) => {
   console.log(error);
-  next(error)
-})
+  next(error);
+});
 
 app.get("/api/products", (req, res) => {
   res.send("Handling product routes.");
