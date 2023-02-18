4f38d04d6016030cbc8e8d7364e57a63c7448597
David Mullally <mullallydavid7@gmail.com>
2023-02-18 20:36:40 +0100

    cmplete first api endpoint categories GET

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,7 +1,12 @@
 const Category = require("../models/CategoryModel")
 
-const getCategories = (req, res) => {
-    res.send("Handling category routes, e.g search for categories")
+const getCategories = async(req, res, next) => {
+    try {
+        const categories = await Category.find({}).sort({ name: "asc" }).orFail()
+        res.json(categories)
+    } catch (error) {
+        next(error)
+    }
 }
 
 module.exports = getCategories;
