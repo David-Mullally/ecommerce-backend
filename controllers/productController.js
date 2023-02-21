237fd5b713ee9006093733d383fa46ac002a87f2
David Mullally <mullallydavid7@gmail.com>
2023-02-21 22:55:46 +0100

    start developing pagination functionality

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,7 +3,12 @@ const Product = require("../models/ProductModel")
 
 const getProducts = async(req, res, next) => {
     try {
-        const products = await Product.find({}).sort({ name: 1 }).limit(recordsPerPage)
+        const pageNum = Number(req.query.pageNum) || 1
+        //res.json({pageNum})
+        const products = await Product.find({})
+            .skip(recordsPerPage * (pageNum - 1))
+            .sort({ name: 1 })
+            .limit(recordsPerPage)
         res.json({products})
     } catch (error) {
         next(error)
