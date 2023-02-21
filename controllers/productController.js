76d5c568cfbe3a5a0a05aa290185e2538f85d374
David Mullally <mullallydavid7@gmail.com>
2023-02-21 23:01:39 +0100

    finish databse query for pagination functionality

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,18 +1,23 @@
-const recordsPerPage = require("../config/pagination")
-const Product = require("../models/ProductModel")
+const recordsPerPage = require("../config/pagination");
+const Product = require("../models/ProductModel");
 
-const getProducts = async(req, res, next) => {
-    try {
-        const pageNum = Number(req.query.pageNum) || 1
-        //res.json({pageNum})
-        const products = await Product.find({})
-            .skip(recordsPerPage * (pageNum - 1))
-            .sort({ name: 1 })
-            .limit(recordsPerPage)
-        res.json({products})
-    } catch (error) {
-        next(error)
-    }
-}
+const getProducts = async (req, res, next) => {
+  try {
+    const pageNum = Number(req.query.pageNum) || 1;
+    const totalProducts = await Product.countDocuments({});
+    //res.json({pageNum})
+    const products = await Product.find({})
+      .skip(recordsPerPage * (pageNum - 1))
+      .sort({ name: 1 })
+      .limit(recordsPerPage);
+    res.json({
+      products,
+      pageNum,
+      paginationLinksNumber: Math.ceil(totalProducts / recordsPerPage),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = getProducts;
