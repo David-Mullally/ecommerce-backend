fa7ee6a3f70b7773364c7c7cafe2cabc256a272b
David Mullally <mullallydavid7@gmail.com>
2023-02-21 23:58:52 +0100

    add querys for sorting products

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,11 +4,20 @@ const Product = require("../models/ProductModel");
 const getProducts = async (req, res, next) => {
   try {
     const pageNum = Number(req.query.pageNum) || 1;
-    const totalProducts = await Product.countDocuments({});
+      const totalProducts = await Product.countDocuments({});
     //res.json({pageNum})
+      // sort by name, price etc based on input on frontend
+      let sort ={};
+      const sortOption = req.query.sort || ""
+      //console.log(sortOption)
+      if (sortOption) {
+          let sortOpt = sortOption.split("_")
+          sort = { [sortOpt[0]]: Number(sortOpt[1]) }  // NOTE: [] used here to create dynamic key name
+          //console.log(sort)
+      }
     const products = await Product.find({})
       .skip(recordsPerPage * (pageNum - 1))
-      .sort({ name: 1 })
+      .sort(sort)
       .limit(recordsPerPage);
     res.json({
       products,
