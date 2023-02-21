b601ae511dd2b6d7f4ab7a76b190faf0b7c3fbd9
David Mullally <mullallydavid7@gmail.com>
2023-02-22 00:21:09 +0100

    add product filter query

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,19 +3,42 @@ const Product = require("../models/ProductModel");
 
 const getProducts = async (req, res, next) => {
   try {
+    let queryCondition = false;
+    let query = {};
+    let priceQueryCondition = {};
+
+    if (req.query.price) {
+      queryCondition = true;
+      priceQueryCondition = { price: { $lte: req.query.price } };
+    }
+
+    let ratingQueryCondition = {};
+    if (req.query.rating) {
+      queryCondition = true;
+      ratingQueryCondition = { rating: { $in: req.query.rating.split(",") } };
+    }
+
+    if (!queryCondition) {
+      query = {
+        $and: [priceQueryCondition, ratingQueryCondition],
+      };
+    }
+
+    //pagination
+
     const pageNum = Number(req.query.pageNum) || 1;
-      const totalProducts = await Product.countDocuments({});
     //res.json({pageNum})
-      // sort by name, price etc based on input on frontend
-      let sort ={};
-      const sortOption = req.query.sort || ""
-      //console.log(sortOption)
-      if (sortOption) {
-          let sortOpt = sortOption.split("_")
-          sort = { [sortOpt[0]]: Number(sortOpt[1]) }  // NOTE: [] used here to create dynamic key name
-          //console.log(sort)
-      }
-    const products = await Product.find({})
+    // sort by name, price etc based on input on frontend
+    let sort = {};
+    const sortOption = req.query.sort || "";
+    //console.log(sortOption)
+    if (sortOption) {
+      let sortOpt = sortOption.split("_");
+      sort = { [sortOpt[0]]: Number(sortOpt[1]) }; // NOTE: [] used here to create dynamic key name
+      //console.log(sort)
+    }
+    const totalProducts = await Product.countDocuments({});
+    const products = await Product.find(query)
       .skip(recordsPerPage * (pageNum - 1))
       .sort(sort)
       .limit(recordsPerPage);
