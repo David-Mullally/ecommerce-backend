831230f33773ef569f1a2913be491b58e54cce91
David Mullally <mullallydavid7@gmail.com>
2023-02-22 13:38:18 +0100

    add db query for filtering products by category

--- controllers/productController.js
+++ controllers/productController.js
@@ -16,22 +16,39 @@ const getProducts = async (req, res, next) => {
     if (req.query.rating) {
       queryCondition = true;
       ratingQueryCondition = { rating: { $in: req.query.rating.split(",") } };
+    }
+
+    let categoryQueryCondition = {};
+    const categoryName = req.params.categoryName || "";
+    console.log(categoryName);
+    if (categoryName) {
+      queryCondition = true;
+      let a = categoryName.replaceAll(",", "/");
+      var regEx = new RegExp("^" + a);
+      categoryQueryCondition = { category: regEx };
+      console.log(regEx);
+    }
+
+  
+    if (req.query.category) {
+      queryCondition = true;
+      let a = req.query.category.split(",").map((item) => {
+        if(item) return new RegExp("^" + item)
+      })
+       console.log(a)
+      categoryQueryCondition = {
+        category: { $in: a }
       }
-      
-      let categoryQueryCondition = {}
-      const categoryName = req.params.categoryName || ""
-      console.log(categoryName)
-      if (categoryName) {
-          queryCondition = true
-          let a = categoryName.replaceAll(",", "/")
-          var regEx = new RegExp("^" + a)
-          categoryQueryCondition = { category: regEx }
-          console.log(regEx)
-      }
+      console.log(categoryQueryCondition)
+    }
 
     if (queryCondition) {
       query = {
-        $and: [priceQueryCondition, ratingQueryCondition, categoryQueryCondition],
+        $and: [
+          priceQueryCondition,
+          ratingQueryCondition,
+          categoryQueryCondition,
+        ],
       };
     }
 
