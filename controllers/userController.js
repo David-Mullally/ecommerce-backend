7b4b86621487be49373d41d5da8b2dbf69ec34be
Dave Mullally <mullallydavid7@gmail.com>
2023-03-02 19:06:50 +0100

    create cookies on  user registraton and send with response

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,34 +1,53 @@
-const User = require("../models/UserModel")
-const { hashPassword } = require("../utils/hashPassword")
+const User = require("../models/UserModel");
+const { hashPassword } = require("../utils/hashPassword");
 
 const getUsers = async (req, res, next) => {
-    try {
-        const users = await User.find({}).select("-password")
-        return res.json(users)
-    } catch (error) {
-        next(error)
-   }
-}
+  try {
+    const users = await User.find({}).select("-password");
+    return res.json(users);
+  } catch (error) {
+    next(error);
+  }
+};
 
 const registerUser = async (req, res, next) => {
-    try {
-        const { name, lastName, email, password } = req.body
-        if (!(name && lastName && email && password)) {
-            return res.status(400).send("All fields are required")
-        }
-
-        const userExists = await User.findOne({ email })
-        if (userExists) {
-            return res.status(400).json({error: "user already exists"})
-        } else {
-            const hashedPassword = hashPassword(password)
-            const user = await User.create({
-                name, lastName, email: email.toLowerCase(), password: hashedPassword
-            })
-            res.status(201).send(user)
-        }
-    } catch (error) {
-        next(error)
+  try {
+    const { name, lastName, email, password } = req.body;
+    if (!(name && lastName && email && password)) {
+      return res.status(400).send("All fields are required");
     }
-}
-module.exports = {getUsers, registerUser};
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).send("user already exists");
+    } else {
+      const hashedPassword = hashPassword(password);
+      const user = await User.create({
+        name,
+        lastName,
+        email: email.toLowerCase(),
+        password: hashedPassword,
+      });
+      res
+        .cookie("access_token", "fake access token", {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: "strict",
+        })
+        .status(201)
+        .json({
+          success: "user created successfully",
+          userCreated: {
+            _id: user._id,
+            name: user.name,
+            lastName: user.lastName,
+            email: user.email,
+            isAdmin: user.isAdmin,
+          },
+        });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports = { getUsers, registerUser };
