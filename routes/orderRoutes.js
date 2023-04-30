f57777ed2522fca85f2ee54497c192bfef68b724
Dave Mullally <mullallydavid7@gmail.com>
2023-04-30 11:03:23 +0200

    add enpoint to update order to paid

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,16 +1,24 @@
-const express = require('express')
-const router = express.Router()
-const { verifyIsLoggedIn, verifyIsAdmin} = require("../middleware/verifyAuthToken")
-const {getUserOrders, getOrder, createOrder} =require("../controllers/orderController")
-
+const express = require("express");
+const router = express.Router();
+const {
+  verifyIsLoggedIn,
+  verifyIsAdmin,
+} = require("../middleware/verifyAuthToken");
+const {
+  getUserOrders,
+  getOrder,
+  createOrder,
+  updateOrderToPaid,
+} = require("../controllers/orderController");
 
 // user routes
-router.use(verifyIsLoggedIn)
-router.get("/", getUserOrders)
-router.get("/user/:id", getOrder)
-router.post("/", createOrder)
+router.use(verifyIsLoggedIn);
+router.get("/", getUserOrders);
+router.get("/user/:id", getOrder);
+router.post("/", createOrder);
+router.patch("/paid/:id", updateOrderToPaid);
 
-//admin routes 
-router.use(verifyIsAdmin)
+//admin routes
+router.use(verifyIsAdmin);
 
-module.exports = router
+module.exports = router;
