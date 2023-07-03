031cebb37d0c2bffda7593aa7677e014cbefe767
Dave Mullally <mullallydavid7@gmail.com>
2023-07-03 22:33:55 +0200

    Send Message From Admin To Client

--- server.js
+++ server.js
@@ -19,45 +19,50 @@ io.on("connection", (socket) => {
       message: msg,
     });
   });
-});
-
-const apiRoutes = require("./routes/apiRoutes");
-
-app.get("/", async (req, res, next) => {
-  try {
-  } catch (err) {
-    next(err);
-  }
-  res.json({ message: "API running..." });
-});
-
-//mongodb connection
-const connectDB = require("./config/db");
-connectDB();
-
-app.use("/api", apiRoutes);
-
-//show errors in the console
-app.use((error, req, res, next) => {
-  if (process.env.NODE_ENV === "development") {
-    console.log(error);
-  }
-  next(error);
-});
-
-app.use((error, req, res, next) => {
-  if (process.env.NODE_ENV === "development") {
-    res.status(500).json({
-      message: error.message,
-      stack: error.stack,
+  socket.on("admin sends message", ({message}) => {
+    socket.broadcast.emit("server sends message from admin to client", {
+      message: message,
     });
-  } else {
-    res.status(500).json({
-      message: error.message,
-    });
-  }
-});
+  });
+})
+  
+  const apiRoutes = require("./routes/apiRoutes");
 
-const PORT = process.env.PORT || 5000;
+  app.get("/", async (req, res, next) => {
+    try {
+    } catch (err) {
+      next(err);
+    }
+    res.json({ message: "API running..." });
+  });
 
-httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  //mongodb connection
+  const connectDB = require("./config/db");
+  connectDB();
+
+  app.use("/api", apiRoutes);
+
+  //show errors in the console
+  app.use((error, req, res, next) => {
+    if (process.env.NODE_ENV === "development") {
+      console.log(error);
+    }
+    next(error);
+  });
+
+  app.use((error, req, res, next) => {
+    if (process.env.NODE_ENV === "development") {
+      res.status(500).json({
+        message: error.message,
+        stack: error.stack,
+      });
+    } else {
+      res.status(500).json({
+        message: error.message,
+      });
+
+    }
+  });
+  const PORT = process.env.PORT || 5000;
+
+  httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
